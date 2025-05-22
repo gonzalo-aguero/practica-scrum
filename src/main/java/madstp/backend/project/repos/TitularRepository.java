84e677325e6fec587a107eb8416bd9f36a15e4ce
FranciscoKuchen1 <franciscokuchen1@gmail.com>
2025-05-22 15:35:16 -0300

    Commit

--- src/main/java/madstp/backend/project/repos/TitularRepository.java
+++ src/main/java/madstp/backend/project/repos/TitularRepository.java
@@ -1,11 +1,16 @@
 package madstp.backend.project.repos;
 
 import madstp.backend.project.domain.Titular;
+import madstp.backend.project.model.TipoDocumento;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 
 public interface TitularRepository extends JpaRepository<Titular, Long> {
 
     boolean existsByDocumentoIgnoreCase(String documento);
+
+    Optional<Titular> findByTipoDocumentoAndDocumento(TipoDocumento tipoDocumento, String documento);
 
 }
